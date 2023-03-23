780fa56c6f95a5aeb64fde659214b366ab7a2b7f
Ashutosh Jha <ashutoshj887@gmail.com>
2023-03-24 04:18:51 +0530

    add: proposal screen

--- data/proposalData.js
+++ data/proposalData.js
@@ -4,12 +4,14 @@ const proposalData = [
         proposalHeading: 'BEING AN AID TO CANCER PATIENTS',
         proposalType: 'Healthcare',
         discussionsCount: 4,
+        maybe: true,
         proposalDetails: 'this proposal discusses the idea of donating some money to the low income cancer patients'
     }, {
         id: 2,
         proposalHeading: 'HELPING IN ESTABLISHING AGRICULTURE INFRASTRUCTURE',
         proposalType: 'Agriculture',
         discussionsCount: 3,
+        maybe: true,
         proposalDetails: 'Investing and upgrading irrigation system for underdeveloped nations'
     },
     {
@@ -17,6 +19,7 @@ const proposalData = [
         proposalHeading: 'Helping refugee around the globe',
         proposalType: 'Social work',
         discussionsCount: 7,
+        maybe: false,
         proposalDetails: 'Donating money for the disaster stricken people including wars, natural calamities etc.'
     },
     {
@@ -24,12 +27,14 @@ const proposalData = [
         proposalHeading: 'Helping refugee around the globe',
         proposalType: 'Social work',
         discussionsCount: 7,
+        maybe: true,
         proposalDetails: 'Donating money for the disaster stricken people including wars, natural calamities etc.'
     }, {
         id: 5,
         proposalHeading: 'Donating funds to poor people strugling with covid ',
         proposalType: 'Healthcare',
         discussionsCount: 7,
+        maybe: true,
         proposalDetails: 'Providing financial assistance to economically weak individuals who are struggling with the impacts of COVID-19 '
     }
 ]
--- screens/ProposalsScreen.js
+++ screens/ProposalsScreen.js
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import proposalData from '../data/proposalData'
+import { Entypo } from '@expo/vector-icons'; 
+
 
 const ProposalsScreen = () => {
   return (
-    <View>
-      <Text>ProposalsScreen</Text>
+    <View style={{ padding: 10 }}>
+      <Text style={{ fontSize: 18, fontWeight: '700', textDecorationLine: 'underline' }}>Trending discussions in teh Community:</Text>
+
+      <View style={{ margin: 10 }}>
+        {proposalData.map((data, index) =>
+          <View style={{ height: 135, marginTop: 5, borderWidth: 1, padding: 5, borderRadius: 6, flexDirection: 'column', padding: 15 }} key={index}>
+            <Text style={{ fontWeight: '700', fontSize: 16 }}>{`#${data.id} ${data.proposalHeading}`}</Text>
+            <Text style={{fontSize: 15}}>Description: {data.proposalDetails}</Text>
+            <Text style={{ fontWeight: '500' }}>Category: {data.proposalType}</Text>
+
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: "space-between", marginTop: 5 }}>
+              <Text style={{ fontWeight: '600' }}>Discussions: {data.discussionsCount}</Text>
+              <Text>Expected: {data.maybe ? 
+                <Entypo name="squared-plus" size={20} color="green" />
+                : 
+                <Entypo name="squared-cross" size={20} color="red" />
+            }</Text>
+            </View>
+          </View>
+        )}
+      </View>
     </View>
   )
 }
