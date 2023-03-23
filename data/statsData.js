3c29a98d87574bc8fd3bdd09cdbf0c412608163c
Ashutosh Jha <ashutoshj887@gmail.com>
2023-03-24 03:27:15 +0530

    update: mock data

--- data/statsData.js
+++ data/statsData.js
@@ -1,44 +1,42 @@
-// mock stats data
-// company name
-// coins redeemed 
-// time: 14months
-
 const statsData = [
     {
         rank: 1,
+        company: 'HCL',
         months: 24,
         totalPoints: 120000
     },
     {
         rank: 2,
+        company: 'Godrej',
         months: 20,
         totalPoints: 100000
 
     },
     {
         rank: 3,
+        company: 'Larsen & Turbo',
         months: 18,
         totalPoints: 90600
     },
-
     {
         rank: 4,
+        company: 'Bisleri',
         months: 17,
         totalPoints: 90500
     },
-
     {
         rank: 5,
+        company: 'Johnson & Johnson',
         months: 14,
         totalPoints: 8900
     },
-
-
     {
         rank: 6,
+        company: 'Hindustan Unilever',
         months: 10,
         totalPoints: 6900
     }
 
 ]
+
 export default statsData;
