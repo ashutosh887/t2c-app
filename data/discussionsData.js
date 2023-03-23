3c1787ecbeebb0c812f1fcb119f47c95d7ac9d11
Ashutosh Jha <ashutoshj887@gmail.com>
2023-03-24 04:06:01 +0530

    add: discussions

---
+++ data/discussionsData.js
@@ -0,0 +1,37 @@
+const discussionsData = [
+    {
+        id: 1,
+        heading: 'BEING AN AID TO CANCER PATIENTS',
+        category: 'Healthcare',
+        conversations: 4,
+        description: 'this proposal discusses the idea of donating some money to the low income cancer patients'
+    }, {
+        id: 2,
+        heading: 'HELPING IN ESTABLISHING AGRICULTURE INFRASTRUCTURE',
+        category: 'Agriculture',
+        conversations: 3,
+        description: 'Investing and upgrading irrigation system for underdeveloped nations'
+    },
+    {
+        id: 3,
+        heading: 'Helping refugee around the globe',
+        category: 'Social work',
+        conversations: 7,
+        description: 'Donating money for the disaster stricken people including wars, natural calamities etc.'
+    },
+    {
+        id: 4,
+        heading: 'Helping refugee around the globe',
+        category: 'Social work',
+        conversations: 7,
+        description: 'Donating money for the disaster stricken people including wars, natural calamities etc.'
+    }, {
+        id: 5,
+        heading: 'Donating funds to poor people strugling with covid ',
+        category: 'Healthcare',
+        conversations: 7,
+        description: 'Providing financial assistance to economically weak individuals, struggling with the impacts of COVID-19'
+    }
+]
+
+export default discussionsData;
--- screens/DiscussionsScreen.js
+++ screens/DiscussionsScreen.js
@@ -1,10 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import discussionsData from '../data/discussionsData'
 
 const DiscussionsScreen = () => {
   return (
-    <View>
-      <Text>DiscussionsScreen</Text>
+    <View style={{ padding: 10 }}>
+      <Text style={{ fontSize: 18, fontWeight: '700', textDecorationLine: 'underline' }}>Trending discussions in teh Community:</Text>
+
+      <View style={{ margin: 10 }}>
+        {discussionsData.map((data, index) =>
+          <View style={{ height: 120, marginTop: 5, borderWidth: 1, padding: 5, borderRadius: 6, flexDirection: 'column', padding: 15 }} key={index}>
+            <Text style={{fontWeight: '700', fontSize: 15}}>{`#${data.id} ${data.heading}`}</Text>
+            <Text>Description: {data.description}</Text>
+
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: "space-between", marginTop: 10}}>
+              <Text style={{ fontWeight: '600' }}>Conversations: {data.conversations}</Text>
+              <Text style={{fontWeight: '600'}}>Category: {data.category}</Text>
+            </View>
+          </View>
+        )}
+      </View>
     </View>
   )
 }
