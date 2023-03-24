27fd00bd91b1958f657c6a89f593d9613c7e139d
Ashutosh Jha <ashutoshj887@gmail.com>
2023-03-24 05:39:14 +0530

    add: rewards screen

--- screens/RewardsScreen.js
+++ screens/RewardsScreen.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Text } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+
+import rewardsData from '../data/rewardsData';
 
 const RewardsScreen = () => {
+  const tableData = rewardsData.map(data => {
+    return [data.coins, data.reward]
+  })
+  const state = {
+    tableHead: ['Tokens', 'Rewards'],
+    tableData: [...tableData]
+  }
+
   return (
-    <View>
-      <Text>RewardsScreen</Text>
+    <View style={styles.container}>
+      <Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+        <Row data={state.tableHead} style={styles.head} />
+        <Rows data={state.tableData} textStyle={styles.text} />
+      </Table>
+
+      <View style={{ flex: 1, paddingTop: 20, backgroundColor: '#fff' }}>
+        <Text style={{fontWeight: '400', padding: 5 }}>*You may avail the benefits of listed rewards in exchange of the following number of tokens. Offer lasts until sponsored. Hurry!</Text>
+      </View>
     </View>
   )
 }
 
 export default RewardsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 10, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+});
